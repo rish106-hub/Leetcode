573a46869bc36e33624b41a852da309673919861
ris106-hub <rishavdewan10@gmail.com>
2025-01-02 11:27:16 +0530

    Base 7

---
+++ base7.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return "0"
+        result = []
+        negative = num < 0
+        num = abs(num)
+        while num > 0:
+            result.append(str(num % 7))
+            num //= 7
+        if negative:
+            result.append('-')
+        return ''.join(result[::-1])
+
+        
